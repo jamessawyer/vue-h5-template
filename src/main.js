08a257c130c673196536b24c9a04aa981bd6c0c5
jinx <550961784@qq.com>
2021-12-06 17:25:17 +0800

    feat: 添加vconsole到项目

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import Vue from 'vue'
 import store from '@/store'
 import router from './router'
 import App from './App.vue'
+import VConsole from 'vconsole'
 
 Vue.config.productionTip = false
+
+// 生产环境去掉console
+if (process.env.NODE_ENV === 'production') {
+  window.console = {
+    info: () => {},
+    log: () => {},
+    warn: () => {},
+    debug: () => {},
+    error: () => {}
+  }
+}
+
+// 开发环境开启vconsole
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line no-unused-vars
+  const vConsole = new VConsole()
+}
 
 new Vue({
   store,
