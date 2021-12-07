94c7030779cf19c0d5730932acb48ce457c369b3
jinx <550961784@qq.com>
2021-12-07 14:42:15 +0800

    chore: App.vue添加一些测试用例

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,44 @@
       <router-link to="/">Go to Home</router-link>
       <router-link to="/about">Go to about</router-link>
     </p>
+    <van-button type="primary" size="small">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger" @click="handleClick">危险按钮</van-button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { add } from '@utils/time.js'
+import { Button } from 'vant'
+import Request from '@utils/request'
 
 export default {
-  components: {},
+  components: {
+    [Button.name]: Button
+  },
   computed: {},
+  created() {
+    Request.get('todos/1').then((res) => {
+      console.log('res: ' + res)
+    })
+  },
   mounted() {
     console.log('VUE_APP_SERVER_URL')
     console.log(process.env.VUE_APP_SERVER_URL)
     console.log('----------------------------------------')
-    console.log('NODE_ENV', add(20, 20))
     console.log(process.env.NODE_ENV)
+  },
+  methods: {
+    handleClick() {
+      this.$toast('点击了')
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -33,5 +50,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  h1 {
+    font-size: 24px;
+  }
 }
 </style>
