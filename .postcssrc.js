d49eab17b998490158ed7d70a2a5af8cd7d654f5
jinx <550961784@qq.com>
2021-12-07 09:42:41 +0800

    feat: 添加Vant UI

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}, // 用来给不同的浏览器自动添加相应前缀，如-webkit-，-moz-等等
     "postcss-px-to-viewport": {
       unitToConvert: "px", // 要转化的单位
       viewportWidth: 375, // UI设计稿的宽度
